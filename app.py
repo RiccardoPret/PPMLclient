04753fe5c5a944cfbb4f7174abc2e41fb213e1c1
Pret <riky.pret@gmail.com>
2017-03-02 03:35:10 +0200

    Added classification using GET methods for request and response

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import pandas
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
@@ -9,23 +9,41 @@ import urllib.request
 
 app = Flask(__name__)
 
+def permListify(s_perms):
+	#multiple apps list permissions
+	ps=[]
+	for p in s_perms.split(','):
+		tmp=[]
+		for sp in p:
+			#permissions of single app
+			tmp.append(sp)
+		ps.append(tmp)
+	return ps
+
 @app.route('/')
 def hello():
-	
-	#Get the dataset
-	features = urllib.request.urlopen("https://rikyupl.000webhostapp.com/features.txt")
-	dataset= pandas.read_csv(features)
+	all_args = request.args.to_dict()
+	try:
+		perm=all_args['perm']
+	except:
+		return "Incorrect URL"
 
-	#Split the features from the label
-	X = np.array(dataset.drop(['label'], 1))
-	y = np.array(dataset['label'])
-	
+	permlist = permListify(perm)
+	for app_perm in permlist:
+		if len(app_perm) != 20:
+			return "Incorrect permission"
+
+	# Get the classifier
 	response = urllib.request.urlopen("https://rikyupl.000webhostapp.com/clf.pickle")
 	data = response.read()
 	clf = pickle.loads(data)
 
-	accuracy = clf.score(X, y)
-	return str(accuracy)
+	# Predict the result
+	prediction=clf.predict(permlist)
+	result=""
+	for r in prediction:
+		result = result + str(r) + ','
+	return str(result[:-1])
 
 if __name__ == '__main__':
-    app.run()
+	app.run()
